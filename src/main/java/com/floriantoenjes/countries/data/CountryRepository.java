e238f7e9efe479deb3ac66a504fcec955e77d19a
Florian <skatepainter@yahoo.de>
2016-07-22 09:05:00 +0200

    Minor changes.

--- src/main/java/com/floriantoenjes/countries/data/CountryRepository.java
+++ src/main/java/com/floriantoenjes/countries/data/CountryRepository.java
@@ -10,12 +10,12 @@ import java.util.List;
 @Component
 public class CountryRepository {
     private static final List<Country> ALL_COUNTRIES = new ArrayList<>(Arrays.asList(
-            new Country("Cameroon", 20_549_221, "Yaoundé", new String[]{"French", "English"}),
             new Country("Albania", 2_800_138, "Tirana", new String[]{"Albanian"}),
-            new Country("Romania", 19_870_000, "Bukarest", new String[]{"Romanian"}),
+            new Country("Cameroon", 20_549_221, "Yaoundé", new String[]{"French", "English"}),
             new Country("Italy", 60_665_551, "Rome", new String[]{"Italian"}),
-            new Country("Malaysia", 28_334_135, "Kuala Lumpur", new String[]{"Malaysian"})
-    ));
+            new Country("Malaysia", 28_334_135, "Kuala Lumpur", new String[]{"Malaysian"}),
+            new Country("Romania", 19_870_000, "Bukarest", new String[]{"Romanian"})
+            ));
 
     public static List<Country> getAllCountries() {
         return ALL_COUNTRIES;
